dd00b57276577ff9f0240afc08ba0af9548360a7
ekalinauskas <ekalinauskas@gmail.com>
2022-05-13 11:55:02 +0300

    first commit

---
+++ TestAutomationHomeworkVS22/WebDriver.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+
+namespace TestAutomationHomework
+{
+    public class Tests
+    {
+        private IWebDriver Driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+        }
+    }
+}
